f74da9997f0f476754407f16458f197b751d49b2
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-07-29 16:39:31 +0530

    Add Sorting Functionality

--- script.js
+++ script.js
@@ -62,10 +62,13 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 // Display Movements
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = ``;
 
-  movements.forEach(function (mov, i) {
+  // Sorting in Ascending Order if sort = true
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -225,4 +228,13 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
+
 // ////////////////////////////////////////////////////////////////////////////////////////
+
